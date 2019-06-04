d1677fa5a5385a95f95ed69e0d2847066976534e
Masha <mashabudryte@gmail.com>
2019-06-04 21:08:44 +0100

    control unit receives instructions and launches squad

--- lib/control_unit.rb
+++ lib/control_unit.rb
@@ -1,9 +1,11 @@
 require_relative 'input_parser'
+require_relative 'rover'
 class Control_unit
   attr_reader :instructions_string, :instructions_array
   def initialize
     @instructions_string = ""
     @instructions_array = []
+    @output = ""
   end
 
   def load_instructions(string)
@@ -13,5 +15,16 @@ class Control_unit
   def parse_instructions(parser = Input_parser.new(@instructions_string))
     parser.save_instructions
     @instructions_array = parser.output
+  end
+
+  def launch_squad
+
+    @instructions_array.each do |instruction|
+      rover = Rover.new(instruction)
+      rover.travel
+      @output == "" ? @output += rover.report_location : @output += "\n#{rover.report_location}"
+    end
+
+    @output
   end
 end
