2ff81db9ebd1fb2202f7f6e0249f862145807b50
Masha <mashabudryte@gmail.com>
2019-06-04 15:48:47 +0100

    Input parser returns instructions in form of the hash

--- lib/input_parser.rb
+++ lib/input_parser.rb
@@ -9,22 +9,26 @@ class Input_parser
     line_splitter[0]
   end
 
-  def no_of_rovers
-    @input.count("\n") / 2
-  end
-
   def save_instructions
-    @instructions << {
-      x: line_splitter[1].split(' ')[0].to_i,
-      y: line_splitter[1].split(' ')[1].to_i,
-      direction: line_splitter[1].split(' ')[2],
-      route: line_splitter[2]
-    }
+    i = 1
+    while i <= no_of_lines
+      @instructions << {
+        x: line_splitter[i].split(' ')[0].to_i,
+        y: line_splitter[i].split(' ')[1].to_i,
+        direction: line_splitter[i].split(' ')[2],
+        route: line_splitter[i + 1]
+      }
+      i += 2
+    end
   end
 
   private
 
   def line_splitter
     @input.split(/\n/)
+  end
+
+  def no_of_lines
+    @input.count("\n")
   end
 end
--- spec/input_parser_spec.rb
+++ spec/input_parser_spec.rb
@@ -8,14 +8,6 @@ describe Input_parser do
     end
   end
 
-  describe '#no_of_rovers' do
-    subject(:input_parser) { described_class.new("5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM") }
-
-    it 'gets info on the no of rovers' do
-      expect(input_parser.no_of_rovers).to eq(2)
-    end
-  end
-
   describe '#save_instructions' do
     subject(:input_parser) { described_class.new("5 5\n1 2 N\nLMLMLMLMM") }
     it 'instructions saved for one rover' do
@@ -31,23 +23,24 @@ describe Input_parser do
     end
   end
 
-  # describe 'acceptance criteria' do
-  #   subject(:input_parser){ described_class.new("5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM") }
-  #   it 'returns a hash with instructions' do
-  #     expect(input_parser.instructions).to eq([
-  #       {
-  #       :direction => 'N',
-  #       :x => 1,
-  #       :y => 2,
-  #       :route => 'LMLMLMLMM'
-  #     },
-  #     {
-  #       :direction => 'E',
-  #       :x => 3,
-  #       :y => 3,
-  #       :route => 'MMRMMRMRRM'
-  #     }
-  #     ])
-  #   end
-  # end
+  describe 'acceptance criteria' do
+    subject(:input_parser){ described_class.new("5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM") }
+    it 'returns a hash with instructions' do
+      input_parser.save_instructions
+      expect(input_parser.instructions).to eq([
+        {
+        :direction => 'N',
+        :x => 1,
+        :y => 2,
+        :route => 'LMLMLMLMM'
+      },
+      {
+        :direction => 'E',
+        :x => 3,
+        :y => 3,
+        :route => 'MMRMMRMRRM'
+      }
+      ])
+    end
+  end
 end
